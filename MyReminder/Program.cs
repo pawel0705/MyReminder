97ceebd295d9c45255574fd23ba835a54d05300d
Paweł Salicki <pawel.salicki@gmail.com>
2023-03-12 20:48:20 +0100

    added angular base project

--- MyReminder/Program.cs
+++ MyReminder/Program.cs
@@ -76,35 +76,6 @@ finally
     Log.CloseAndFlush();
 }
 
-
-/*
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-*/
-
 static void AppUncatchedExceptionHandler(object sender, UnhandledExceptionEventArgs eventArgs)
 {
     var exception = (Exception)eventArgs.ExceptionObject;
